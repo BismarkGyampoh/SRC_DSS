0609068fbb317d6437df254017d9878c256ce07c
BismarkGyampoh <bismarkgyampoh1@gmail.com>
2026-07-19 13:39:22 +0000

    Delete DSS directory

--- DSS/docs/database_update.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE projects MODIFY COLUMN dss_status ENUM('Pending', 'Accepted', 'Rejected', 'Deferred') NOT NULL DEFAULT 'Pending';
--- DSS/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Landing page: public transparency dashboard
-header('Location: views/public_dashboard.php', true, 302);
-exit();
